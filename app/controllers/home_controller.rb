56df3bc5ffccdadea30cc9d67edf061cc49980f7
priyank rege <priyankrege@gmail.com>
2019-09-22 02:46:54 -0500

    Final Commit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,16 +1,26 @@
-require 'net/http'
-require 'json'
-
 class HomeController < ApplicationController
   def index
-    url = 'http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=75024&distance=25&API_KEY=4C27472A-75A8-451F-B1D7-E31F5DFEF596'
+    get_weather
+  end
+
+  def zipcode
+    @zipcode_value = params[:zipcode]
+    return @zipcode_value ='No zipcode' if params[:zipcode].blank?
+    get_weather
+  end
+
+  private
+
+  def get_weather
+    require 'net/http'
+    require 'json'
+    value = @zipcode_value.blank? ? '78741' : @zipcode_value
+    url = 'http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode='+value+'&distance=25&API_KEY=4C27472A-75A8-451F-B1D7-E31F5DFEF596'
     response = Net::HTTP.get(URI(url))
     @output = JSON.parse(response).empty? ? 'Error' : JSON.parse(response)[0]['AQI']
     change_background_color_and_description
     @output
   end
-
-  private
 
   def change_background_color_and_description
     if @output != 'Error'
